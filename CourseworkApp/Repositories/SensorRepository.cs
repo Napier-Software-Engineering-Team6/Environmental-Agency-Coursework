06943b260cd3c7a4b7c620cc69a869558b57cca4
Dantano89 <Daniel_celentano@hotmail.co.uk>
2025-04-26 18:08:23 +0100

    feat: expanded & personalized mock sensor data with famous video game locations

--- CourseworkApp/Repositories/SensorRepository.cs
+++ CourseworkApp/Repositories/SensorRepository.cs
@@ -14,10 +14,13 @@ namespace CourseworkApp.Repositories
             // Mock some sensor data
             _mockSensors = new List<SensorModel>
             {
-                new SensorModel { Id = 1, Name = "Air Sensor 1", Location = "Racoon City", Status = "Active", LastUpdated = DateTime.Now.AddMinutes(-5), Type = "Air" },
-                new SensorModel { Id = 2, Name = "Water Sensor 1", Location = "Zanarkand", Status = "Inactive", LastUpdated = DateTime.Now.AddMinutes(-20), Type = "Water" },
-                new SensorModel { Id = 3, Name = "Weather Station 1", Location = "Deepnest", Status = "Malfunction", LastUpdated = DateTime.Now.AddMinutes(-10), Type = "Weather" },
-                new SensorModel { Id = 4, Name = "Air Sensor 2", Location = "Namek", Status = "Active", LastUpdated = DateTime.Now.AddMinutes(-2), Type = "Air" },
+            new SensorModel { Id = 1, Name = "Air Sensor 1", Location = "Racoon City", Status = "Active", LastUpdated = DateTime.Now.AddMinutes(-5), Type = "Air" },
+            new SensorModel { Id = 2, Name = "Water Sensor 1", Location = "Deepnest", Status = "Inactive", LastUpdated = DateTime.Now.AddMinutes(-20), Type = "Water" },
+            new SensorModel { Id = 3, Name = "Weather Station 1", Location = "Zanarkand", Status = "Malfunction", LastUpdated = DateTime.Now.AddMinutes(-10), Type = "Weather" },
+            new SensorModel { Id = 4, Name = "Air Sensor 2", Location = "Namek", Status = "Active", LastUpdated = DateTime.Now.AddMinutes(-2), Type = "Air" },
+            new SensorModel { Id = 5, Name = "Water Sensor 2", Location = "Pallet Town", Status = "Malfunction", LastUpdated = DateTime.Now.AddMinutes(-30), Type = "Water" },
+            new SensorModel { Id = 6, Name = "Air Sensor 3", Location = "Chemical Plant", Status = "Inactive", LastUpdated = DateTime.Now.AddMinutes(-15), Type = "Air" },
+            new SensorModel { Id = 7, Name = "Weather Station 2", Location = "Hyrule", Status = "Active", LastUpdated = DateTime.Now.AddMinutes(-8), Type = "Weather" },
             };
         }
 
