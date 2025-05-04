c10680983c18d0c46872a8f57a00918e148f5eab
owen <owenshaw90d@gmail.com>
2025-05-04 21:06:43 +0100

    feat:adding marker to the map

--- CourseworkApp/ViewModels/SensorMapViewModel.cs
+++ CourseworkApp/ViewModels/SensorMapViewModel.cs
@@ -5,15 +5,5 @@ namespace CourseworkApp.ViewModels;
 
 public partial class SensorMapViewModel : ObservableObject
 {
-  [ObservableProperty]
-  private Location initialLocation;
 
-  [ObservableProperty]
-  private Distance initialRadius;
-
-  public SensorMapViewModel()
-  {
-    InitialLocation = new Location(55.9486, -3.2021);
-    InitialRadius = Distance.FromKilometers(2);
-  }
 }
